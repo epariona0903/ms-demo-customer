8085fed31f922a19264b2746bca5bd0803aa8e6f
epariona0903 <epariona0903@gmail.com>
2025-03-04 16:55:24 -0500

    Update HomeController.java

--- src/main/java/com/reto/HomeController.java
+++ src/main/java/com/reto/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    public password = "VbhpJgFkEc4rk86cpZhWKV";
+    public String password = "VbhpJgFkEc4rk86cpZhWKV";
         
     @GetMapping("/")
     public String helloWorld() {
